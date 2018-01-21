8609def44d312639efd0a2fc18dc89e592a4bd20
Bill Erickson <bill.erickson@gmail.com>
2018-01-21 09:22:46 -0600

    subpages backend render works

--- blocks/list-subpages/list-subpages.js
+++ blocks/list-subpages/list-subpages.js
@@ -2,44 +2,40 @@
  * External dependencies
  */
 import { get } from 'lodash';
-
 /**
  * WordPress dependencies
  */
 const { __ } = wp.i18n;
 const { withAPIData } = wp.components;
+const { Component } = wp.element;
+const { decodeEntities } = wp.utils;
 
-function ListSubpages( { pages, className, parentId } ) {
 
-//	const pageItems = get( pages, 'data', [] );
+class ListSubpages extends Component {
+	constructor() {
+		super( ...arguments );
+	}
 
-	const postsCollection = new wp.api.collections.Pages();
-	const pageItems = postsCollection.fetch( {
-		data: {
-			per_page: 5,
-			parent: parentId,
-		},
-	} );
-
-	console.log( pageItems );
-
-	return '<p>Test</p>';
-
+	render() {
+		const pages = get( this.props.pages, 'data', [] );
+		return [
+				<ul
+					className={ this.props.className }
+				>
+					{ pages.map( ( post, i ) =>
+						<li key={ i }>
+							<a href={ post.link } target="_blank">{ decodeEntities( post.title.rendered.trim() ) || __( '(Untitled)' ) }</a>
+						</li>
+					) }
+				</ul>,
+		];
+	};
 }
 
-
-/*
-const applyWithAPIData = withAPIData( (props) => {
-	const query = JSON.stringify( {
-		per_page: 100,
-		parent: props.parentId,
-		exclude: props.parentId,
-		_fields: [ 'title' ],
-	} );
+const applyWithAPIData = withAPIData( ( props ) => {
 	return {
-		pages: `/wp/v2/pages?${ query }`,
+		pages: `/wp/v2/pages?per_page=100&parent=${props.parentId}&orderby=menu_order&order=asc`,
 	};
 } );
-*/
 
-export default ListSubpages;
+export default applyWithAPIData( ListSubpages );
